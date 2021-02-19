48d07fb345db94d256cd0369d0ab5c14f824ff37
Chris <milple1742>
2021-02-19 13:05:25 +0900

    Initial commit

---
+++ mltk/_min_dependencies.py
@@ -0,0 +1,6 @@
+SKLEARN_MIN_VERSION = "0.24.1"
+
+
+dependent_packages = {
+    "scikit-learn": ()
+}
---
+++ mltk/ensemble/_forest.py
@@ -0,0 +1,6 @@
+from sklearn.ensemble import RandomForestClassifier as RFC
+from sklearn.preprocessing import OneHotEncoder as OHE
+
+class RandomForestClassifier(RFC):
+
+
